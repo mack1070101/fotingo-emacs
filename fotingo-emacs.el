012597a2b89497ca7d2f5505b4ab53a181ab32af
Bligh <mackenziebligh@gmail.com>
2019-12-24 14:54:34 -0700

    master: Init project
    .gitignore:
    - Add gitignore
    
    fotingo-emacs:
    - Stub code file

---
+++ fotingo-emacs.el
@@ -0,0 +1,26 @@
+;;; fotingo-emacs.el --- An Emacs wrapper for fotingo  -*- lexical-binding: t; coding: utf-8 -*-
+
+;; Author: Mackenzie Bligh <mackenziebligh@gmail.com>
+
+;; Keywords: git tools vc jira
+;; Homepage: TODO
+
+;; fotingo-emacs requires at least GNU Emacs 25.1 and fotingo TODO
+
+;; fotingo-emacs is free software; you can redistribute it and/or modify it
+;; under the terms of the gnu general public license as published by
+;; the free software foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; fotingo-emacs is distributed in the hope that it will be useful, but without
+;; any warranty; without even the implied warranty of merchantability
+;; or fitness for a particular purpose.  see the gnu general public
+;; license for more details.
+;;
+;; you should have received a copy of the gnu general public license
+;; along with magit.  if not, see http://www.gnu.org/licenses.
+
+;;; Commentary:
+;; TODO
+
+;; Code:
